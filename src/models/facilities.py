8bacf3ad57df7f4f432bfbe3bb7fc752f58ec91e
sarahhlandis <sarahlandis95@gmail.com>
2023-03-12 15:28:07 +1100

    udpated hours_of_op variable in facilities to opening & closing_time

--- src/models/facilities.py
+++ src/models/facilities.py
@@ -1,5 +1,6 @@
 from datetime import time
 from models import facility_amenities
+from models.facility_types import FacilityType
 from app import db
 
 class Facility(db.Model):
@@ -16,26 +17,26 @@ class Facility(db.Model):
     closing_time = db.Column(db.Time(), nullable=False)
 
     # Add the foreign keys in the Facilities model
-    facility_type_id = db.Column(db.Integer, db.ForeignKey("facility_types.id"), nullable=True, ondelete='SETNULL')
-    owner_id = db.Column(db.Integer, db.ForeignKey("owners.id"), nullable=False)
+    facility_type_id = db.Column(db.Integer, db.ForeignKey("facility_type.id"), nullable=True, ondelete='SETNULL')
+    owner_id = db.Column(db.Integer, db.ForeignKey("owner.id"), nullable=False)
     address_id = db.Column(db.Integer, db.ForeignKey("addresses.id"), nullable=False)
     
     # Add the relationships directions to other models
     promotions = db.relationship('Promotion', backref='facility', lazy=True)
     address = db.relationship('Address', backref='facility')
-    owner = db.relationship('Owner', backref='facilities')
-    facility_type = db.relationship('FacilityType', backref='facility')
+    owner = db.relationship("Owner", backref=db.backref("facilities", cascade="all, delete-orphan"))
+    facility_type = db.relationship('FacilityType', backref=db.backref('facilities', lazy=True))
 
     # Set up join table with amenities
     amenities = db.relationship('Amenity', secondary=facility_amenities, lazy='subquery', backref=db.backref('facilities', lazy='dynamic'))
 
 
-    def __init__(self, phone_num, independent, business_name, hours_of_op, facility_type_id, owner_id, address_id):
+    def __init__(self, phone_num, independent, business_name, hours_of_op, facility_type, owner_id, address_id):
         self.phone_num = phone_num
         self.independent = independent
         self.business_name = business_name
         self.opening_time = time(hour=hours_of_op.hour, minute=hours_of_op.minute)
         self.closing_time = time(hour=hours_of_op.hour, minute=hours_of_op.minute)
-        self.facility_type_id = facility_type_id
+        self.facility_type = FacilityType.query.filter_by(facility_type=facility_type).first()
         self.owner_id = owner_id
         self.address_id = address_id
